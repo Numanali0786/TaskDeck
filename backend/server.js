e2b5f9b0c34664b6d6c2120b00c18d2a1b58eefb
Numan Ali <numan4599322ali@gmail.com>
2026-08-08 06:33:54 +0530

    fix 2

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,9 @@ app.use(
   }),
 );
 
-app.use(express.json({ limit: "1mb" }));
+//apex-ai-crm.onrender.com/
+
+https: app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
